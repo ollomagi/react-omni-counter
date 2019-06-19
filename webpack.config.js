1a469b61953d492df61625ce92853b5f1f1821dd
ollomagi <ollomagi@gmail.com>
2019-06-18 21:00:20 -0700

    Configurate webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  externals: {
+    react: 'commonjs react',
+  },
+}
