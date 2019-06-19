19b34957bbf5a486115bb042ee6a4496589acb11
ollomagi <ollomagi@gmail.com>
2019-06-18 21:20:53 -0700

    Renaming build to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
@@ -12,7 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|build)/,
+        exclude: /(node_modules|dist)/,
         use: {
           loader: 'babel-loader',
           options: {
