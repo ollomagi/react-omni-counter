6daa8c17ca4578bfea8c289c02ef2ad884df327f
ollomagi <ollomagi@gmail.com>
2019-07-14 23:08:13 -0700

    Add Jest

--- src/utils.test.js
+++ src/utils.test.js
@@ -5,3 +5,9 @@ test('format time should add 0 to number smaller than 10 and return a string', (
   expect(formatTime(9)).toBe('09')
   expect(formatTime('8')).toBe('08')
 })
+
+test('format unit should not format time', () => {
+  expect(formatUnit(1, false, 'h', 'full')).toBe('1 hour')
+  expect(formatUnit('2', false, 'h', 'full')).toBe('2 hours')
+  expect(formatUnit('3', false, 'h', 'short')).toBe('3h')
+})
